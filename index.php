95e1a7850503339c40033b11773b8f2dbedb0efc
YongYong37 <2302441g@student.tp.edu.sg>
2025-01-07 16:20:49 +0800

    small edit

--- index.php
+++ index.php
@@ -1,45 +1,20 @@
 <?php
-// STILL IN PROGRESS; Can fix if you want 🙂
-#$connect=mysqli_connect("localhost","root","","Assignment");
-if (isset($_POST['login-button'])) {
-    $username=$_POST["username"];
-    $password=$_POST["password"];
-    $hashed_password=hash("sha256", $password);
-    $query = $connect->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
-    $query->bind_param("ss", $username, $hashed_password);
 
-    $query->execute();
-
-    $result = $query->get_result();
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="styles.css">
-    <title>Login</title>
+    <title>Landing page</title>
 </head>
 <body>
     <div class="top-center">
-        <h1>Login Page</h1>
+        <h1>Advanced Manufacturing Centre</h1>
     </div>
-    <form method="post" action="index.php">
-        <table align="center" border="0">
-            <tr>
-                <td>Username:</td>
-                <td><input type="text" name="username" placeholder="Username" required></td>
-            </tr>
-            <tr>
-                <td>Password:</td>
-                <td><input type="text" name="password" placeholder="Password" required></td>
-            </tr>
-            <tr>
-                <td colspan="2" align="center">
-                    <button type="submit" name="login-button">Log In</button>
-                </td>
-            </tr>
-        </table>
-    </form>
+    <div class="center">
+        <button onclick="window.location.href='http://localhost//xampp/p06_grp2/student-login.php';">Student Login</button>
+        <button onclick="window.location.href='http://localhost//xampp/p06_grp2/staff-login.php';">Staff Login</button>
+    </div>
 </body>
 </html>
