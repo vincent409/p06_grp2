789321ef7ff38f8a2dbca84791aa7f2994054612
vincent409 <2304581H@student.tp.edu.sg>
2025-02-01 03:15:15 +0800

    updated decrypt to ensure it 16

--- database/test.php
+++ database/test.php
@@ -32,6 +32,9 @@
         $iv_length = openssl_cipher_iv_length('aes-256-cbc');
         $iv = substr($data, 0, $iv_length);
         $ciphertext = substr($data, $iv_length);
+        if (strlen($iv) !== $iv_length) {
+            $iv = str_pad($iv, $iv_length, "\0"); // Pad IV with null bytes if too short
+        }
 
         // Decrypt the data
         $decrypted_data = openssl_decrypt($ciphertext, 'aes-256-cbc', $key, 0, $iv);
